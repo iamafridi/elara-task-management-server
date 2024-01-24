8ab394ea67376f4d6e44c04c8a94f20a3ac36d12
Afridi Akbar Ifty <afridiakbarifty@gmail.com>
2024-01-24 11:37:45 +0600

    implemented Cors and dotenv

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
 const express = require("express");
 const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 const jwt = require("jsonwebtoken");
+const app = express();
 const cookieParser = require("cookie-parser");
 const cors = require("cors");
-const app = express();
+require('dotenv').config();
+
 const port = 5000;
 
 const secret ="xyzyzxmbauebaoefh43234325bjhhbi";
@@ -11,10 +13,10 @@ const secret ="xyzyzxmbauebaoefh43234325bjhhbi";
 // Parser/ middleware
 app.use(express.json());
 app.use(cookieParser());
-app.use({
+app.use(cors({
   origin: "http://localhost:5173",
   credentials: true,
-});
+}));
 // DB URI
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.7grn8zj.mongodb.net/?retryWrites=true&w=majority`;
 
